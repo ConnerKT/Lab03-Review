393b67d7344848730b70714889e7c74dc5343179
ConnerKT <connerthompson121@gmail.com>
2023-07-16 21:44:03 -0500

    finished lab 03

--- Lab03-Review/Program.cs
+++ Lab03-Review/Program.cs
@@ -1,9 +1,322 @@
-﻿namespace Lab03_Review;
-class Program
+﻿using System;
+
+namespace Lab03_Review
 {
-    static void Main(string[] args)
+    using System;
+    using System.Reflection.Metadata;
+
+    namespace Lab03_Review
     {
-        Console.WriteLine("Hello, World!");
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                bool selection = false;
+
+                while (selection == false)
+                {
+                    Console.WriteLine("Welcome to my Lab 3 Review, here you can test my code and make sure I know something!");
+
+                    //Stole this sleep idea from Jared :)
+                    Console.WriteLine("Please press a key to continue...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    // Choices for the program
+                    Console.WriteLine("Please Select a Following Option");
+                    Console.WriteLine("Enter 1 to run Challenge 1");
+                    Console.WriteLine("Enter 2 to run Challenge 2");
+                    Console.WriteLine("Enter 3 to run Challenge 3");
+                    Console.WriteLine("Enter 4 to run Challenge 4");
+                    Console.WriteLine("Enter 5 to run Challenge 5");
+                    Console.WriteLine("Enter 6 to run Challenge 6");
+                    Console.WriteLine("Enter 7 to run Challenge 7");
+                    Console.WriteLine("Enter 8 to run Challenge 8");
+                    Console.WriteLine("Enter 9 to run Challenge 9");
+                    Console.WriteLine("Enter 10 to exit :(");
+
+                    string selectionInput = Console.ReadLine();
+
+                    if (selectionInput == "1")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Please enter 3 numbers separated by spaces:");
+                        string input = Console.ReadLine();
+                        int product = Challenge1(input);
+                        
+                        Console.WriteLine("Product: " + product);
+                        Thread.Sleep(500);
+                    }
+                    else if (selectionInput == "2")
+                    {
+                        //Challenge 2
+                        Console.Clear();
+                        Console.WriteLine("Please Enter a number between 2-10: ");
+                        int input = Convert.ToInt32(Console.ReadLine());
+                        int[] array = new int[input];
+                        for (int i = 0; i < input; i++)
+                        {
+                            Console.WriteLine(i + 1 + " of " + input + " - Enter a Number: ");
+                            array[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        int product = Challenge2(array, input);
+                        
+                        Console.WriteLine("Your average between your number is : " + product);
+                        Thread.Sleep(500);
+                    }
+                    else if (selectionInput == "3")
+                    {
+                        Challenge3();
+                    }
+                    else if (selectionInput == "4")
+                    {
+                        Console.WriteLine("Lets find the mode of your numbers!");
+                        Console.WriteLine("Enter multiple numbers separated by spaces:");
+                        string input = Console.ReadLine();
+
+                        // Split the input string into an array of numbers
+                        string[] numberStrings = input.Split(' ');
+                        int[] numbers = Array.ConvertAll(numberStrings, int.Parse);
+
+                        int result = Challenge4(numbers);
+                        Console.WriteLine("The number that appeared the most times is: " + result);
+                    }
+                    else if (selectionInput == "5")
+                    {
+                        Console.WriteLine("Let sort your array!");
+                        Console.WriteLine("Enter multiple numbers separated by spaces:");
+                        string input = Console.ReadLine();
+
+                        // Split the input string into an array of numbers
+                        string[] numberStrings = input.Split(' ');
+                        int[] numbers = Array.ConvertAll(numberStrings, int.Parse);
+                        int result = Challenge5(numbers);
+                        Console.WriteLine("Your maximum value is : " + result);
+                        
+                    }
+                    else if (selectionInput == "6")
+                    {
+                        Console.WriteLine("Input a word to put into the file words.txt: ");
+                        string input = Console.ReadLine();
+                        Challenge6(input);
+                    }
+                    else if (selectionInput == "7")
+                    {
+                        Console.WriteLine("Reading Your File....");
+                        Thread.Sleep(1500);
+                        string results = Challenge7();
+                        Console.WriteLine(results);
+                    }
+
+                    else if (selectionInput == "8")
+                    {
+                        string content = File.ReadAllText("/Users/connerthompson/Documents/Coding Central/GitHub/Lab03-Review/Lab03-Review/words.txt");
+                        Console.WriteLine("This is the content in your file, what would you like to remove?");
+                        Console.WriteLine(content);
+                        string userInput = Console.ReadLine();
+                        Challenge8(userInput);
+                    }
+                    else if (selectionInput == "9")
+                    {
+                        Console.WriteLine("Enter a sentence:");
+                        string input = Console.ReadLine();
+                        string[] array = Challenge9(input);
+                        Console.WriteLine("Word Lengths:");
+                        foreach (string word in array)
+                        {
+                            Console.WriteLine(word);
+                        }
+                    }
+                    else if (selectionInput == "10")
+                    {
+                        selection = true;
+                    }
+                }
+            }
+
+            public static int Challenge1(string input)
+            {
+                string[] numbers = input.Split(' ');
+
+                if (numbers.Length < 3)
+                    return 0;
+
+                int product = 1;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i >= numbers.Length || !int.TryParse(numbers[i], out int number))
+                        number = 1;
+
+                    product *= number;
+                }
+
+                return product;
+            }
+            public static int Challenge2(int[] array, int input)
+            {
+                int total = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    total += array[i];
+                }
+                int value = total / input;
+
+                return value;
+            }
+            public static void Challenge3()
+            {
+                int size = 9;
+
+                int middleRow = (size - 1) / 2;
+                for (int i = 0; i <= middleRow; i++)
+                {
+                  
+                    for (int j = 0; j < middleRow - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 0; j < 2 * i + 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                for (int i = middleRow - 1; i >= 0; i--)
+                {
+     
+                    for (int j = 0; j < middleRow - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 0; j < 2 * i + 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            public static int Challenge4(int[] array)
+            {
+                Dictionary<int, int> numberCounts = new Dictionary<int, int>();
+
+                foreach (int number in array)
+                {
+                    if (numberCounts.ContainsKey(number))
+                    {
+                        numberCounts[number]++;
+                    }
+                    else
+                    {
+                        numberCounts[number] = 1;
+                    }
+                }
+
+                int maxCount = 0;
+                int mostFrequentNumber = array[0]; 
+
+                foreach (KeyValuePair<int, int> kvp in numberCounts)
+                {
+                    if (kvp.Value > maxCount)
+                    {
+                        maxCount = kvp.Value;
+                        mostFrequentNumber = kvp.Key;
+                    }
+                }
+
+                return mostFrequentNumber;
+            }
+            public static int Challenge5(int[] array)
+            {
+                bool switched;
+                int greatestNumber = 0;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    switched = false;
+
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            int temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                            switched = true;
+                        }
+                    }
+                    if (!switched)
+                    {
+                        //Break because we want out if its already been swapped
+                        break;
+                    }
+                for (int k = 0; k < array.Length; k++)
+                    {
+                        
+                        if (array[k] > greatestNumber)
+                        {
+                            greatestNumber = array[k];
+                        }
+
+                    }
+
+                }
+
+                return greatestNumber;
+            }
+            public static void Challenge6(string input)
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter("/Users/connerthompson/Documents/Coding Central/GitHub/Lab03-Review/Lab03-Review/words.txt"))
+                    {
+                        writer.Write(input);
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Error: File path not found");
+                }
+            }
+            public static string Challenge7()
+            {
+                string content = File.ReadAllText("/Users/connerthompson/Documents/Coding Central/GitHub/Lab03-Review/Lab03-Review/words.txt");
+                return content;
+            }
+            public static void Challenge8(string word)
+            {
+                string filePath = "/Users/connerthompson/Documents/Coding Central/GitHub/Lab03-Review/Lab03-Review/words.txt";
+                string content = File.ReadAllText(filePath);
+                string updatedContent = content.Replace(word, "");
+                File.WriteAllText(filePath, updatedContent);
+
+            }
+            public static string[] Challenge9(string sentence)
+            {
+                string[] words = sentence.Split(' ');
+                string[] wordLengths = new string[words.Length];
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    string word = words[i];
+                    int wordLength = word.Length;
+                    wordLengths[i] = $"{word}: {wordLength}";
+                }
+
+                return wordLengths;
+
+            }
+
+
+
+        }
     }
+
 }
+
 
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,193 @@
+﻿namespace Lab03_Review.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Challenge1_InputStringOfNumbers()
+        {
+        
+            string input = "2 4 6";
+
+            int result = Lab03_Review.Program.Challenge1(input);
+
+            Assert.Equal(48, result);
+        }
+
+        [Fact]
+        public void Challenge1_InputMoreThan3Numbers()
+        {
+         
+            string input = "5 7 9 3";
+
+     
+            int result = Lab03_Review.Program.Challenge1(input);
+
+      
+            Assert.Equal(315, result);
+        }
+
+        [Fact]
+        public void Challenge1_InputLessThan3Numbers()
+        {
+         
+            string input = "1 2";
+
+       
+            
+            int result = Lab03_Review.Program.Challenge1(input);
+
+        
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Challenge1_InputNegativeNumbers()
+        {
+      
+            string input = "-2 -4 -6";
+
+            int result = Lab03_Review.Program.Challenge1(input);
+
+      
+            Assert.Equal(-48, result);
+        }
+        [Fact]
+        public void Challenge2_InputRangeOfNumbers()
+        {
+       
+            int[] array = { 1, 2, 3, 4, 5 };
+            int input = array.Length;
+
+  
+            int result = Lab03_Review.Program.Challenge2(array, input);
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Challenge2_InputSingleNumber()
+        {
+   
+            int[] array = { 7 };
+            int input = array.Length;
+
+     
+            int result = Lab03_Review.Program.Challenge2(array, input);
+
+  
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void Challenge2_InputAllZeros()
+        {
+
+            int[] array = { 0, 0, 0 };
+            int input = array.Length;
+
+  
+            int result = Lab03_Review.Program.Challenge2(array, input);
+
+   
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void Challenge4_InputDifferentArrays()
+        {
+       
+            int[] array1 = { 1, 2, 2, 3, 4, 4, 4 };
+            int[] array2 = { 1, 1, 2, 2, 2, 3, 3, 3, 3 };
+            int[] array3 = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+
+        
+            int result1 = Lab03_Review.Program.Challenge4(array1);
+            int result2 = Lab03_Review.Program.Challenge4(array2);
+            int result3 = Lab03_Review.Program.Challenge4(array3);
+     
+            Assert.Equal(4, result1);
+            Assert.Equal(2, result2);
+            Assert.Equal(5, result3);
+        }
+
+        [Fact]
+        public void Challenge4_InputArrayWithSameNumber()
+        {
+            int[] array = { 3, 3, 3, 3, 3 };
+            int result = Lab03_Review.Program.Challenge4(array);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Challenge4_InputArrayWithNoDuplicates()
+        {
+        
+            int[] array = { 1, 2, 3, 4, 5 };
+            int result = Lab03_Review.Program.Challenge4(array);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Challenge4_InputArrayWithMultipleEF()
+        {
+       
+            int[] array = { 1, 2, 2, 3, 3, 4, 4 };
+            int result = Lab03_Review.Program.Challenge4(array);
+            
+            Assert.Equal(2, result);
+        }
+        [Fact]
+        public void Challenge5_InputArrayWithNegativeNumbers()
+        {
+            int[] array = { -10, -5, -20, -3, -7 };
+
+         
+            int result = Lab03_Review.Program.Challenge5(array);
+
+            Assert.Equal(-3, result);
+        }
+
+        [Fact]
+        public void Challenge5_InputArrayWithAllSameValues()
+        {
+            int[] array = { 7, 7, 7, 7, 7 };
+
+            int result = Lab03_Review.Program.Challenge5(array);
+
+          
+            Assert.Equal(7, result);
+        }
+        [Fact]
+        public void Challenge9_InputSentence()
+        {
+            string input = "This is a sentence about important things";
+            string[] expected = { "This: 4", "is: 2", "a: 1", "sentence: 8", "about: 5", "important: 9", "things: 6" };
+
+            string[] result = Lab03_Review.Program.Challenge9(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Challenge9_ReturnsArrayWithWordLengths()
+        {
+            string input = "The quick brown fox jumps over the lazy dog";
+            string[] expected = { "The: 3", "quick: 5", "brown: 5", "fox: 3", "jumps: 5", "over: 4", "the: 3", "lazy: 4", "dog: 3" };
+
+            string[] result = Lab03_Review.Program.Challenge9(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Challenge9_InputSentenceWithSymbols()
+        {
+            string input = "Hello, world! How are you today?";
+            string[] expected = { "Hello: 5", "world: 5", "How: 3", "are: 3", "you: 3", "today: 5" };
+
+            string[] result = Lab03_Review.Program.Challenge9(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
+
